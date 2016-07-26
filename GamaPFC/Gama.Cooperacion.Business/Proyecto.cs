0fa3af96fcf6ba5ebbb2e3ad826fbdd0346835e0
Alberto Cardona López <cardona.lopez.alberto@gmail.com>
2016-07-26 15:30:24 +0100

    Crear los dos primeros proyectos de el módulo de cooperación y la clase Proyecto

---
+++ GamaPFC/Gama.Cooperacion.Business/Proyecto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gama.Cooperacion.Business
+{
+    public enum Estado
+    {
+        NoComenzado,
+        Comenzado,
+        Finalizado,
+    }
+
+    public enum TipoDeProyecto
+    {
+        Taller,
+        Charla,
+        Conferencia,
+        Manifestacion,
+    }
+
+    public class Proyecto
+    {
+        // Son propiedades porque tienen accesores (getters y setter)
+        public string Descripcion { get; set; }
+        public Estado Estado { get; set; }
+        public DateTime FechaDeInicio { get; set; }
+        public DateTime FechaDeFin { get; set; }
+        public int Id { get; private set; }
+        public TipoDeProyecto Tipo { get; set; }
+        public string Titulo { get; set; }
+    }
+}
