15c308943176623866eddf13e9c67adc3d7f24b9
Alberto Cardona López <cardona.lopez.alberto@gmail.com>
2016-08-10 12:45:18 +0100

    Proyecto se llama ahora Actividad

--- GamaPFC/Gama.Cooperacion.Business/Actividad.cs
+++ GamaPFC/Gama.Cooperacion.Business/Actividad.cs
@@ -12,16 +12,8 @@ namespace Gama.Cooperacion.Business
         Comenzado,
         Finalizado,
     }
-
-    public enum TipoDeProyecto
-    {
-        Taller,
-        Charla,
-        Conferencia,
-        Manifestacion,
-    }
-
-    public class Proyecto
+    
+    public class Actividad
     {
         // Son propiedades porque tienen accesores (getters y setter)
         public string Descripcion { get; set; }
@@ -29,7 +21,6 @@ namespace Gama.Cooperacion.Business
         public DateTime FechaDeInicio { get; set; }
         public DateTime FechaDeFin { get; set; }
         public int Id { get; private set; }
-        public TipoDeProyecto Tipo { get; set; }
         public string Titulo { get; set; }
     }
 }
