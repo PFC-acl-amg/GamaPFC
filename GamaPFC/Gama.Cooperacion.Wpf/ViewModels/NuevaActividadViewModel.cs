79dbf38bc35d20745bedfe8c9d5e2c30ceed0dc1
Alberto Cardona López <cardona.lopez.alberto@gmail.com>
2016-08-10 13:43:39 +0100

    Versión espartana de NuevaActividadViewModel

---
+++ GamaPFC/Gama.Cooperacion.Wpf/ViewModels/NuevaActividadViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Gama.Cooperacion.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gama.Cooperacion.Wpf.ViewModels
+{
+    public class NuevaActividadViewModel
+    {
+        public Actividad Actividad { get; set; }
+
+        public NuevaActividadViewModel()
+        {
+            Actividad = new Actividad();
+            Actividad.Titulo = "Nombre del proyecto...";
+            Actividad.Descripcion = "Descripción del proyecto...";
+        }
+    }
+}
