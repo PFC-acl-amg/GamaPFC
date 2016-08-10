55c331bede7915d92d5ef799d1927a018e5b1051
Alberto Cardona López <cardona.lopez.alberto@gmail.com>
2016-08-10 12:54:43 +0100

    Añadir clase Cooperante y actualizada Actividad para incluirlo

--- GamaPFC/Gama.Cooperacion.Business/Actividad.cs
+++ GamaPFC/Gama.Cooperacion.Business/Actividad.cs
@@ -16,11 +16,14 @@ namespace Gama.Cooperacion.Business
     public class Actividad
     {
         // Son propiedades porque tienen accesores (getters y setter)
+        public Cooperante Coordinador { get; set; }
         public string Descripcion { get; set; }
         public Estado Estado { get; set; }
         public DateTime FechaDeInicio { get; set; }
         public DateTime FechaDeFin { get; set; }
         public int Id { get; private set; }
         public string Titulo { get; set; }
+
+        public List<Cooperante> Cooperantes { get; private set; }
     }
 }
---
+++ GamaPFC/Gama.Cooperacion.Business/Cooperante.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gama.Cooperacion.Business
+{
+    public class Cooperante 
+    {
+        public string Apellido { get; set; }
+        public string Dni { get; set; }
+        public List<string> Email { get; private set; }
+        public int Id { get; private set; }
+        public string Nombre { get; set; }
+        public string Observaciones { get; set; }
+        public List<string> Telefonos { get; private set; }
+    }
+}
