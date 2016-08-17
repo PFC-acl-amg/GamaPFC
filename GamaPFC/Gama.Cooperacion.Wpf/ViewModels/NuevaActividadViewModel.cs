f9aa0bec1a0eefa24900879889d51eedded8c86c
Alberto Cardona López <cardona.lopez.alberto@gmail.com>
2016-08-17 12:16:56 +0100

    Añadir VM y crear comandos

--- GamaPFC/Gama.Cooperacion.Wpf/ViewModels/NuevaActividadViewModel.cs
+++ GamaPFC/Gama.Cooperacion.Wpf/ViewModels/NuevaActividadViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using Core;
 using Gama.Cooperacion.Business;
+using Prism.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Gama.Cooperacion.Wpf.ViewModels
 {
@@ -17,6 +19,21 @@ namespace Gama.Cooperacion.Wpf.ViewModels
             Actividad = new Actividad();
             Actividad.Titulo = "Nombre del proyecto...";
             Actividad.Descripcion = "Descripción del proyecto...";
+
+            AceptarCommand = new DelegateCommand(OnAceptar, OnAceptar_CanExecute);
+        }
+
+        ICommand AceptarCommand { get; set; }
+
+        private void OnAceptar()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool OnAceptar_CanExecute()
+        {
+            return (String.IsNullOrEmpty(Actividad.Titulo) &&
+                String.IsNullOrEmpty(Actividad.Descripcion));
         }
     }
 }
