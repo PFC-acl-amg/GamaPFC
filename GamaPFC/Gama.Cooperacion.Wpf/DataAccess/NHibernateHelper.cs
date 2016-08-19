04a0c747d1507feae4f70cde35544c8be838b244
Alberto Cardona López <cardona.lopez.alberto@gmail.com>
2016-08-19 11:45:45 +0100

    Ultimos proyectos en Dashboard y notificar y actualizarlo cuando se añade evento

--- GamaPFC/Gama.Cooperacion.Wpf/DataAccess/NHibernateHelper.cs
+++ GamaPFC/Gama.Cooperacion.Wpf/DataAccess/NHibernateHelper.cs
@@ -55,7 +55,7 @@ namespace Gama.Cooperacion.Wpf.DataAccess
                             c.SetProperty("current_session_context_class", "thread_static");
                             schema.Execute(
                                 useStdOut: true,
-                                execute: true,
+                                execute: false,
                                 justDrop: false);
                         })
                    .BuildConfiguration();
---
+++ GamaPFC/Gama.Cooperacion.Wpf/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Core;
+using Gama.Cooperacion.Business;
+using Gama.Cooperacion.Wpf.Eventos;
+using Gama.Cooperacion.Wpf.Services;
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gama.Cooperacion.Wpf.ViewModels
+{
+    public class DashboardViewModel : ViewModelBase
+    {
+        private IActividadRepository _actividadRepository;
+        private int _cantidadDeProyectosAMostrar = 5;
+        private IEventAggregator _eventAggregator;
+
+        public ObservableCollection<Actividad> UltimasActividades { get; private set; }
+
+        public DashboardViewModel(IActividadRepository actividadRepository,
+            IEventAggregator eventAggregator)
+        {
+            _actividadRepository = actividadRepository;
+            _eventAggregator = eventAggregator;
+
+            UltimasActividades = new ObservableCollection<Actividad>(
+                _actividadRepository.GetAll()
+                .OrderBy(a => a.FechaDeFin)
+                .Take(_cantidadDeProyectosAMostrar)
+                .ToArray());
+
+            _eventAggregator.GetEvent<NuevaActividadEvent>().Subscribe(OnNuevaActividadEvent);
+        }
+
+        private void OnNuevaActividadEvent(Actividad actividad)
+        {
+            UltimasActividades.Add(actividad);
+        }
+    }
+}
