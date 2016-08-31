948dba0a16c7e06de35327e3d08f2798c14d7951
Alberto Cardona López <cardona.lopez.alberto@gmail.com>
2016-08-31 11:58:22 +0100

    Completar relaciones entre cooperante y actividad y tarea e inicializar listas en Cooperante

--- GamaPFC/Gama.Cooperacion.Business/Cooperante.cs
+++ GamaPFC/Gama.Cooperacion.Business/Cooperante.cs
@@ -8,12 +8,23 @@ namespace Gama.Cooperacion.Business
 {
     public class Cooperante 
     {
+        public IList<Actividad> ActividadesEnQueParticipa { get; protected set; }
         public string Apellido { get; set; }
         public string Dni { get; set; }
-        public IList<string> Email { get; private set; }
+        public IList<string> Emails { get; private set; }
         public int Id { get; private set; }
         public string Nombre { get; set; }
         public string Observaciones { get; set; }
+        public IList<Tarea> TareasEnQueParticipa { get; protected set; }
         public IList<string> Telefonos { get; private set; }
+
+        public Cooperante()
+        {
+            ActividadesEnQueParticipa = new List<Actividad>();
+            Emails = new List<string>();
+            TareasEnQueParticipa = new List<Tarea>();
+            Telefonos = new List<string>();
+        }
+
     }
 }
