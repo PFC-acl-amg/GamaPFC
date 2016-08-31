b77957ac1f257157bb09f5b42c94ec51d3455f9f
Alberto Cardona López <cardona.lopez.alberto@gmail.com>
2016-08-31 11:53:48 +0100

    Cambiar de List a IList en Cooperante

--- GamaPFC/Gama.Cooperacion.Business/Cooperante.cs
+++ GamaPFC/Gama.Cooperacion.Business/Cooperante.cs
@@ -10,10 +10,10 @@ namespace Gama.Cooperacion.Business
     {
         public string Apellido { get; set; }
         public string Dni { get; set; }
-        public List<string> Email { get; private set; }
+        public IList<string> Email { get; private set; }
         public int Id { get; private set; }
         public string Nombre { get; set; }
         public string Observaciones { get; set; }
-        public List<string> Telefonos { get; private set; }
+        public IList<string> Telefonos { get; private set; }
     }
 }
