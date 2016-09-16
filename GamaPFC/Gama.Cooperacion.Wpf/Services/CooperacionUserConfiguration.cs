378c34746e66d21182b9b8275e3c7a7f83d3c190
Alberto Cardona López <cardona.lopez.alberto@gmail.com>
2016-09-16 13:14:32 +0100

    cooperante user config

---
+++ GamaPFC/Gama.Cooperacion.Wpf/Services/CooperacionUserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gama.Cooperacion.Wpf.Services
+{
+    public class CooperacionUserConfiguration : ICooperacionUserConfiguration
+    {
+        public CooperacionUserConfiguration()
+        {
+            ListadoDeActividadesItemsPerPage = 30;
+        }
+
+        public int ListadoDeActividadesItemsPerPage { get; set; }
+    }
+}
