f386336331be1871b837ff49ec6ec81cb541c029
Alberto Cardona López <cardona.lopez.alberto@gmail.com>
2016-10-02 16:30:28 +0100

    dependencias para módulos nuevos

---
+++ GamaPFC/Gama.Atenciones.Wpf/AtencionesModule.cs
@@ -0,0 +1,49 @@
+﻿using Core;
+using Microsoft.Practices.Unity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gama.Atenciones.Wpf
+{
+    public class AtencionesModule : ModuleBase
+    {
+        public AtencionesModule(IUnityContainer container, IRegionManager regionManager)
+           : base(container, regionManager)
+        {
+            this.Entorno = Entorno.Desarrollo;
+            this.UseFaker = false;
+        }
+
+        public override void Initialize()
+        {
+            RegisterViews();
+            RegisterViewModels();
+            RegisterServices();
+            InitializeNavigation();
+        }
+
+        private void RegisterViews()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RegisterViewModels()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RegisterServices()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void InitializeNavigation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GamaPFC/Gama.Atenciones.WpfTests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gama.Atenciones.WpfTests
+{
+    public class Class1
+    {
+    }
+}
