84bbf64eb1f4c582cd898dd856685cb44494ac3e
Alberto Cardona López <cardona.lopez.alberto@gmail.com>
2016-11-21 21:04:58 +0000

    Primera versión del CustomCalendar (añadir untracked files)

---
+++ GamaPFC/Gama.Atenciones.Wpf/Controls/Day.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Gama.Atenciones.Wpf.Controls
+{
+    public class Day : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private DateTime date;
+        private string notes;
+        private bool enabled;
+        private bool isTargetMonth;
+        private bool isToday;
+
+        public bool IsToday
+        {
+            get { return isToday; }
+            set
+            {
+                isToday = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("IsToday"));
+            }
+        }
+
+        public bool IsTargetMonth
+        {
+            get { return isTargetMonth; }
+            set
+            {
+                isTargetMonth = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("IsTargetMonth"));
+            }
+        }
+
+        public bool Enabled
+        {
+            get { return enabled; }
+            set
+            {
+                enabled = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("Enabled"));
+            }
+        }
+
+        public string Notes
+        {
+            get { return notes; }
+            set
+            {
+                notes = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("Notes"));
+            }
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set
+            {
+                date = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("Date"));
+            }
+        }
+    }
+}
---
+++ GamaPFC/Gama.Atenciones.Wpf/ViewModels/NuevaCitaViewModel.cs
@@ -0,0 +1,65 @@
+﻿using Core;
+using Gama.Atenciones.Business;
+using Gama.Atenciones.Wpf.Wrappers;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Gama.Atenciones.Wpf.ViewModels
+{
+    public class NuevaCitaViewModel : ViewModelBase
+    {
+        private bool? _Cerrar; // Debe ser nulo al inicializarse el VM o hay excepciones por DialogCloser
+
+        public NuevaCitaViewModel()
+        {
+            Cita = new CitaWrapper(new Cita());
+
+            AceptarCommand = new DelegateCommand(OnAceptarCommand_Execute,
+                OnAceptarCommand_CanExecute);
+            CancelarCommand = new DelegateCommand(OnCancelarCommand_Execute);
+
+            Cita.PropertyChanged += Cita_PropertyChanged;
+        }
+
+        private void Cita_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            ((DelegateCommand)AceptarCommand).RaiseCanExecuteChanged();
+        }
+
+        public CitaWrapper Cita { get; private set; }
+
+        public bool? Cerrar
+        {
+            get { return _Cerrar; }
+            set { SetProperty(ref _Cerrar, value); }
+        }
+
+        public ICommand AceptarCommand { get; private set; }
+        public ICommand CancelarCommand { get; private set; }
+
+        public void Load(PersonaWrapper persona)
+        {
+            Cita.Persona = persona;
+        }
+
+        private void OnAceptarCommand_Execute()
+        {
+            Cerrar = true;
+        }
+
+        private bool OnAceptarCommand_CanExecute()
+        {
+            return Cita.Inicio != null && Cita.Asistente != null;
+        }
+
+        private void OnCancelarCommand_Execute()
+        {
+            Cerrar = true;
+        }
+    }
+}
