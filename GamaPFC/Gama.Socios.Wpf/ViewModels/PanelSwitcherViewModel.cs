17949b30bf844245d027636f307c4567a727543d
Alberto Cardona López <cardona.lopez.alberto@gmail.com>
2016-12-04 09:47:23 +0000

    PanelSwitcher

---
+++ GamaPFC/Gama.Socios.Wpf/ViewModels/PanelSwitcherViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Core;
+using Gama.Common;
+using Prism.Commands;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Gama.Socios.Wpf.ViewModels
+{
+    public class PanelSwitcherViewModel : ViewModelBase
+    {
+        private IRegionManager _RegionManager;
+
+        public PanelSwitcherViewModel(IRegionManager regionManager)
+        {
+            _RegionManager = regionManager;
+
+            this.NavigateCommand = new DelegateCommand<string>(Navigate);
+        }
+
+        private void Navigate(string viewName)
+        {
+            _RegionManager.RequestNavigate(RegionNames.ContentRegion, viewName);
+        }
+
+        public ICommand NavigateCommand { get; private set; }
+    }
+}
