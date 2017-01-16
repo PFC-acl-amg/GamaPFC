9bf078e00d81130678f76966038a8321e7120322
Alberto Cardona López <cardona.lopez.alberto@gmail.com>
2017-01-16 14:30:33 +0000

    empezar custom control para botón con icono

---
+++ GamaPFC/Gama.Common/CustomControls/CircleIconButton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Gama.Common.CustomControls
+{
+    public class CircleIconButton : Button
+    {
+        public Visual Icon
+        {
+            get { return (Visual)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(Visual), typeof(CircleIconButton), new PropertyMetadata(default(Visual)));
+
+    }
+}
