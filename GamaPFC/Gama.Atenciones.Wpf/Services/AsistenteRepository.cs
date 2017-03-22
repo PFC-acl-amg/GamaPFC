cc458d634ae93a6426c6fbd6d4d645c4b63e3dcd
Alberto <cardona.lopez.alberto@gmail.com>
2017-03-22 18:01:17 +0000

    añadir AsistenteRepository

---
+++ GamaPFC/Gama.Atenciones.Wpf/Services/AsistenteRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.DataAccess;
+using Gama.Atenciones.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gama.Atenciones.Wpf.Services
+{
+    public class AsistenteRepository :
+        NHibernateOneSessionRepository<Asistente, int>,
+        IAsistenteRepository
+    {
+    }
+}
