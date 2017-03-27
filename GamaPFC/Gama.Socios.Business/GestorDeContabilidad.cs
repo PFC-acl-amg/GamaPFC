22c79fef5b4d6af9464384c4f69b2ffa67be97bc
JoseAMG2014 <jose.sl500@gmail.com>
2017-03-27 18:36:53 +0100

    Contabilidad Border

--- GamaPFC/Gama.Socios.Business/GestorDeContabilidad.cs
+++ GamaPFC/Gama.Socios.Business/GestorDeContabilidad.cs
@@ -1,16 +1,43 @@
-﻿using System;
+﻿using Core;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
+
 namespace Gama.Socios.Business
 {
-    public class GestorDeContabilidad
+    public class GestorDeContabilidad:ObservableObject
     {
         public GestorDeContabilidad()
         {
-
+            
         }
+
+        //public virtual ObservableCollection<LookupItemSocio> ListarSocios()
+        // {
+
+        // }
+
+        // Hacer visible el Stack Panel para busrcar por distintos campos en la lista de socios
+        private bool _BuscardorVisible = true;
+        public bool BuscadorVisible
+        {
+            get { return _BuscardorVisible; }
+            set
+            {
+                if (_BuscardorVisible != value)
+                {
+                    _BuscardorVisible = value;
+                    OnPropertyChanged();
+                }
+
+            }
+        }
+        //-----------------
     }
 }
