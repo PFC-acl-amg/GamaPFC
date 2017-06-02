a29907049984d3b71b4f36498243eb5298e4d318
Alberto <cardona.lopez.alberto@gmail.com>
2017-06-02 10:02:02 +0100

    acción en atenciones para reabrir selector de módulo

--- GamaPFC/Gama.Atenciones.Wpf/ViewModels/RightCommandsViewModel.cs
+++ GamaPFC/Gama.Atenciones.Wpf/ViewModels/RightCommandsViewModel.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Gama.Atenciones.Wpf.ViewModels
@@ -21,13 +22,22 @@ namespace Gama.Atenciones.Wpf.ViewModels
             _EventAggregator = eventAggregator;
 
             AbrirPreferenciasCommand = new DelegateCommand(OnAbrirPreferenciasCommandExecute);
+            VolverASeleccionDeModuloCommand = new DelegateCommand(OnVolverASeleccionDeModuloExecute);
         }
 
         public ICommand AbrirPreferenciasCommand { get; private set; }
+        public ICommand VolverASeleccionDeModuloCommand { get; private set; }
 
         private void OnAbrirPreferenciasCommandExecute()
         {
             _EventAggregator.GetEvent<AbrirPreferenciasEvent>().Publish();
+        }
+
+        private void OnVolverASeleccionDeModuloExecute()
+        {
+            System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
+            Application.Current.Shutdown();
+            _EventAggregator.GetEvent<VolverASeleccionDeModuloEvent>().Publish();
         }
     }
 }
--- GamaPFC/Gama.Bootstrapper/SelectorDeModuloViewModel.cs
+++ GamaPFC/Gama.Bootstrapper/SelectorDeModuloViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using Core;
 using Gama.Bootstrapper.Services;
+using Gama.Common.Eventos;
 using Prism.Commands;
+using Prism.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using System.Windows.Threading;
 
@@ -16,23 +19,39 @@ namespace Gama.Bootstrapper
         private LoginService _LoginService;
         private bool? _Cerrar; // Debe ser nulo al inicializarse el VM, o hay excepción con DialogCloser
         private bool _MostrarLogin;
+        private IEventAggregator _EventAggregator;
 
-        public SelectorDeModuloViewModel(LoginService loginService)
+        public SelectorDeModuloViewModel(LoginService loginService, IEventAggregator eventAggregator)
         {
             _LoginService = loginService;
+            _EventAggregator = eventAggregator;
 
             SeleccionarModuloCommand = new DelegateCommand<string>(OnSeleccionarModuloCommandExecute);
             AccederCommand = new DelegateCommand(OnAccederCommandExecute);
 
             SeHaAccedido = false;
 
+            Usuario = "admin";
+            Password = "clave";
+            SeleccionarModuloCommand.Execute("atenciones");
+
+            _EventAggregator.GetEvent<VolverASeleccionDeModuloEvent>().Subscribe(OnVolverASeleccionDeModuloEvent);
+
             _Timer = new DispatcherTimer();
             _Timer.Tick += _timer_Tick;
             _Timer.Interval = new TimeSpan(0, 0, 2);
         }
 
+        private void OnVolverASeleccionDeModuloEvent()
+        {
+            VolverASeleccionDeModulo = true;
+            System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
+            //Application.Current.Shutdown();
+        }
+
         public ICommand SeleccionarModuloCommand { get; private set; }
         public ICommand AccederCommand { get; private set; }
+        public bool VolverASeleccionDeModulo { get; set; }
 
         public Modulos ModuloSeleccionado { get; private set; }
 
