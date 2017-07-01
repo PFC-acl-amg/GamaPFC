eb20c4d0ef4da3bcc84140f820dc4b8fb8263f38
Alberto <cardona.lopez.alberto@gmail.com>
2017-07-01 15:25:54 +0100

    add debug

---
+++ GamaPFC/Gama.Common/Debug/Debug.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gama.Common.Debug
+{
+    public static class Debug
+    {
+        public static bool DEBUG = true;
+        private static System.Diagnostics.Stopwatch _Stopwatch;
+
+        public static void StartStopWatch()
+        {
+            if (DEBUG)
+                _Stopwatch = System.Diagnostics.Stopwatch.StartNew();
+        }
+
+        public static void StopWatch(string source)
+        {
+            if (DEBUG)
+            {
+                _Stopwatch.Stop();
+                Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+                Console.WriteLine($">>>>>>>>>>>>>>{source}: {_Stopwatch.ElapsedMilliseconds / 1000.0} segundos");
+                Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            }
+        }
+    }
+}
