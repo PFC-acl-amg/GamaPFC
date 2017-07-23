f0a0e555c23cd08e36dbbfe6073308d005c901f8
Alberto <cardona.lopez.alberto@gmail.com>
2017-07-23 17:03:12 +0100

    label foreground destacado

--- GamaPFC/Gama.Socios.Wpf/ViewModels/ListadoDeSociosViewModel.cs
+++ GamaPFC/Gama.Socios.Wpf/ViewModels/ListadoDeSociosViewModel.cs
@@ -5,6 +5,7 @@ using Gama.Socios.Business;
 using Gama.Socios.Wpf.Eventos;
 using Gama.Socios.Wpf.Services;
 using NHibernate;
+using Prism;
 using Prism.Commands;
 using Prism.Events;
 using System;
@@ -17,7 +18,7 @@ using System.Windows.Input;
 
 namespace Gama.Socios.Wpf.ViewModels
 {
-    public class ListadoDeSociosViewModel : ViewModelBase
+    public class ListadoDeSociosViewModel : ViewModelBase, IActiveAware
     {
         private IEventAggregator _EventAggregator;
         private ISocioRepository _SocioRepository;
@@ -142,6 +143,23 @@ namespace Gama.Socios.Wpf.ViewModels
         private void OnPreferenciasActualizadasEvent()
         {
             Socios.ItemsPerPage = _Preferencias.ListadoDeSociosItemsPerPage;
+        }
+
+        private bool _IsActive = true;
+        public bool IsActive
+        {
+            get { return _IsActive; }
+
+            set
+            {
+                SetProperty(ref _IsActive, value);
+                if (_IsActive)
+                {
+                    // NOTA: Se está usando un 0 (cero) para indicar que ya no hay
+                    // persona seleccionada. Se ha convenido así.
+                    _EventAggregator.GetEvent<SocioSeleccionadoChangedEvent>().Publish(0);
+                }
+            }
         }
     }
 }
